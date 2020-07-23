601f499aed805013513234f398cf5a80da2e58ca
Space School <space.school.kg@gmail.com>
2020-07-23 21:23:14 +0600

    check integrate

--- src/service/idb.js
+++ src/service/idb.js
@@ -1,15 +1,24 @@
-import { openDB } from 'idb';
+import { openDB, deleteDB } from 'idb';
 import { initReceiveData } from './idb/receiveData';
 import { initOfflineOrders } from './idb/offlineOrders';
+import { SingletonStore } from '../singleton/store';
 export let db = undefined
 export let start = async () => {
     if (window.indexedDB) {
-        db = await openDB('b10b11bd-ab56-46e2-99b3-58d4be94a882', 1, {
-            upgrade(db) {
-                initReceiveData(db)
-                initOfflineOrders(db)
-            },
-        });
+        if(new SingletonStore().getStore()&&new SingletonStore().getStore().getState().user.profile.role&&new SingletonStore().getStore().getState().user.profile.role.includes('агент')) {
+            db = await openDB('b10b11bd-ab56-46e2-99b3-58d4be94a882', 1, {
+                upgrade(db) {
+                    initReceiveData(db)
+                    initOfflineOrders(db)
+                },
+            });
+        }
+        else {
+            db = await openDB('b10b11bd-ab56-46e2-99b3-58d4be94a882', 1, {});
+            if(db) {
+                await deleteDB('b10b11bd-ab56-46e2-99b3-58d4be94a882', {});
+            }
+        }
     }
 }
 
